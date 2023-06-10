8e222ca75baf1183c43d8af805a38e9c52c17af0
V0pe <dampblew01@gmail.com>
2023-06-09 23:06:17 -0600

    Add the api call and redux actions

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 const api = {
-    fetchRandomMessage: async () => {
-      const response = await fetch('http://localhost:3000/api/v1/greets');
-      const { text } = await response.json();
-  
-      return text;
-    },
-  };
-  export default api;
+  fetchRandomMessage: async () => {
+    const response = await fetch('http://localhost:3000/api/v1/greets');
+    const { text } = await response.json();
+
+    return text;
+  },
+};
+export default api;
