54155bbd6049a859e4027c0ea39bfa03f9740c7d
V0pe <dampblew01@gmail.com>
2023-06-09 22:58:10 -0600

    add the dependecies

---
+++ src/api/api.js
@@ -0,0 +1,9 @@
+const api = {
+    fetchRandomMessage: async () => {
+      const response = await fetch('http://localhost:3000/api/v1/greets');
+      const { text } = await response.json();
+  
+      return text;
+    },
+  };
+  export default api;
